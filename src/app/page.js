e2e77353f006cf996ab2199a5e31470f5de9b6af
Potdar <aryanpotdar2003@gmail.com>
2024-09-29 21:31:12 +0530

    Fixed News Box and Dashboard css

--- src/app/page.js
+++ src/app/page.js
@@ -7,14 +7,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+      <div className="flex flex-row">
+      <div className="flex flex-col">
       <h1 className="text-3xl ml-10  font-bold ">Indices</h1>    
       <div className="flex flex-row justify-start">
         <Card title="Sensex" content="+5124" Points="Point 1"/>
         <Card title="Nifty 50" content="+200"/>
         <Card title="Nasdaq" content="+12.56 "/>
+        </div>
+        </div>
+        <div>
+        <h1 className="text-3xl ml-10 font-bold ">Stock News</h1>
         <NewsCard title="News 1" content="Content 4" />
         </div>
-        
+        </div>
     </>
   );
 }
--- src/components/tile/newsbox.jsx
+++ src/components/tile/newsbox.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NewsCard = ({ title, content, icon }) => {
   return (
      <div className="flex flex-row justify-start hover:scale-105 duration-300 ease-in">
-    <div className=" flex flex-col ml-10 mt-10 border-4 border-[#FFAA2B] h-80 w-80 rounded-lg p-6 shadow-lg">
+    <div className=" flex flex-col ml-10 mt-5 border-4 border-[#FFAA2B] h-80 w-80 rounded-lg p-6 shadow-lg">
       <h1 className="text-2xl font-bold mb-2">{title}</h1>
       <p className="text-gray-700">{content}</p>
     </div>
