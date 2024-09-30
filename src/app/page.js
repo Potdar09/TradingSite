3d2b586017903afc4fc78c0b1d2782f77c5900dd
CarIsFast <chinmay1raskar@gmail.com>
2024-09-30 14:24:45 +0530

    nav change

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Card from "@/components/cards/indexcard";
 import GainerBox from "@/components/tile/gainerbox/gainer";
-import AreaChart from "@/components/tile/graphbox";
 import LoserBox from "@/components/tile/loserbox/loserbox";
 import NewsCard from "@/components/tile/newsbox";
 
@@ -10,26 +9,19 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-row">
-      <div className="flex flex-col">
-      <h1 className="text-3xl ml-10  font-bold ">Indices</h1>    
-      <div className="flex flex-row justify-start">
-        <Card title="Sensex" content="+5124" Points="Points"/>
-        <Card title="Nifty 50" content="+200"/>
-        <Card title="Nasdaq" content="+12.56 "/>
+        <div className="flex flex-col">
+          <h1 className="text-3xl ml-10  font-bold ">Indices</h1>    
+          <div className="flex flex-row justify-start">
+            <Card title="Sensex" content="+5124" Points="Points"/>
+            <Card title="Nifty 50" content="+200"/>
+            <Card title="Nasdaq" content="+12.56 "/>
+          </div>
         </div>
-        </div>
-        <div>
-        <h1 className="text-3xl ml-10 font-bold ">Stock News</h1>
-        <NewsCard title="News 1" content="Content 4" />
-        <AreaChart />
-        </div>
-        </div>
-        <div className="flex flex-col ">
-        <h1 className="text-3xl ml-10 font-bold ">Top Gainers</h1>    
-        <GainerBox title="Gainers" content="Content 3" />
-        <h1 className="text-3xl ml-10 font-bold mt-5">Top Losers</h1>    
-        <LoserBox title="Losers" content="Content 3" />
-        </div>
+          <div>
+            <h1 className="text-3xl ml-10 font-bold ">Stock News</h1>
+            <NewsCard title="News 1" content="Content 4" />
+          </div>
+      </div>
     </>
   );
 }
