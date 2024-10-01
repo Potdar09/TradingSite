da4f5262c48e0d270bc45d549c133410811a4878
Potdar <aryanpotdar2003@gmail.com>
2024-10-02 01:18:14 +0530

    Dashboard CSS changed. Background and Sidebar Fixed. x-OverFlow Fixed

--- src/app/page.js
+++ src/app/page.js
@@ -14,9 +14,9 @@ export default function Home() {
           <h1 className="text-3xl m-5  font-bold ">Indices</h1>    
           <div className="flex flex-row justify-between ml-5 mr-5">
             
-            <Card title="Sensex" points='52,922.60' dayChange="+100" dayChangePerc="+0.25" open="52700.40" high="52900.40" low="52300.40" low52wk="52300.40"/>
-            <Card title="Nifty 50" points='52,922.60' dayChange="+100" dayChangePerc="+0.25" open="52700.40" high="52900.40" low="52300.40" low52wk="52300.40"/>
-            <Card title="Nasdaq" points='52,922.60' dayChange="+100" dayChangePerc="+0.25" open="52700.40" high="52900.40" low="52300.40" low52wk="52300.40"/>
+            <Card title="Sensex" points='+522.60' dayChange="+100" dayChangePerc="+0.25" open="52700.40" high="52900.40" low="52300.40" low52wk="52300.40"/>
+            <Card title="Nifty 50" points='+522.60' dayChange="+100" dayChangePerc="+0.25" open="52700.40" high="52900.40" low="52300.40" low52wk="52300.40"/>
+            <Card title="Nasdaq" points='+522.60' dayChange="+100" dayChangePerc="+0.25" open="52700.40" high="52900.40" low="52300.40" low52wk="52300.40"/>
           
           </div>
           <h1 className="text-3xl m-5 font-bold ">Top Gainers</h1>    
@@ -24,9 +24,10 @@ export default function Home() {
           <h1 className="text-3xl m-5 font-bold">Top Losers</h1>    
           <LoserBox title="Losers" content="Content 3" />
         </div>
-          <div>
+        <div className="flex flex-col mt-5">
             <h1 className="text-3xl ml-10 font-bold ">Stock News</h1>
             <NewsCard title="News 1" content="Content 4" />
+            <h1 className="text-3xl ml-10 mt-5 font-bold ">Investments</h1>
             <InvestBox title="News 1" content="Content 4"/>
           </div>
       </div>
