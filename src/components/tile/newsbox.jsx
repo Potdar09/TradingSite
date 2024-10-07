a9b803f65f5f81c88f2629eb890b0e2796133403
CarIsFast <chinmay1raskar@gmail.com>
2024-10-07 13:11:15 +0530

    basic news section

--- src/components/tile/newsbox.jsx
+++ src/components/tile/newsbox.jsx
@@ -1,14 +1,58 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 
-const NewsCard = ({ title, content, icon }) => {
+const News = () => {
+  const [newsTitle, setNewsTitle] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // Simulated network request
+      const title = [
+        {
+          id: 1,
+          ticker:'AAPL',
+          title: "Trucking group leader calls for political intervention amid ongoing port strike"
+        },
+        {
+          id: 2,
+          ticker:'TSLA',
+          title: "US offers EVgo conditional $1.05 billion loan for EV chargers"
+        },
+        {
+          id: 3,
+          ticker:'RELA',
+          title: "US announces new EVgo loan for EV chargers"
+        },
+        {
+          id: 4,
+          ticker:'GOOGL',
+          title: "Why Kamala Harris says we need a national reserve for critical minerals"
+        }
+      ];
+
+      setNewsTitle(title);
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
-     <div className="flex flex-row justify-start hover:scale-105 duration-300 ease-in">
-      <div className=" flex flex-col ml-10 mr-10 mt-5 bg-[#16181B] h-80 w-full rounded-lg p-6 shadow-lg">
-      <h1 className="text-2xl font-bold mb-2">{title}</h1>
-      <p className="text-gray-700">{content}</p>
-    </div>
+    <div className="flex flex-row justify-start hover:scale-105 duration-300 ease-in">
+      <div className="flex flex-col ml-10 mr-10 mt-5 bg-[#16181B] h-80 w-full rounded-lg p-3 shadow-lg overflow-hidden">
+        {newsTitle.slice(0,3).map((newsItem) => (
+          <>
+          <div key={newsItem.id} className='text-white font-semibold text-xl p-3 border-b border-[#2b2b32]'>
+            {newsItem.title}
+            <div className='text-white text-[1rem] font-normal border border-[#2b2b32] rounded-xl'>
+              {newsItem.ticker}
+            </div>
+          </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default NewsCard;
+export default News;
